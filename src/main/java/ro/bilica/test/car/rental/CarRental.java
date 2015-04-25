5709188b0674beffbb8932c90208c86d40ed19e4
cornel.bilica@gmail.com <cornel.bilica@gmail.com>
2015-04-25 17:52:49 +0300

    Created a list for rented cars

--- src/main/java/ro/bilica/test/car/rental/CarRental.java
+++ src/main/java/ro/bilica/test/car/rental/CarRental.java
@@ -1,15 +1,21 @@
 package ro.bilica.test.car.rental;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CarRental {
 	
 	private final long maxRentalLength;
+	private Map<String, Long> carToRentalTime;
 	
 	public CarRental(long maxRentalLength) {
 		this.maxRentalLength = maxRentalLength;
+		carToRentalTime = new HashMap<String, Long>();
+
 	}
 	
 	public void rentCar(String registrationNumber) {
-		
+		carToRentalTime.put(registrationNumber, System.currentTimeMillis());
 	}
 	
 	public void returnCar(String registrationNumber) {
